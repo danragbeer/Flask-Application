103004262d82c3a323233129281f9c5958e3858d
Daniel Ragbeer <52211069+danragbeer@users.noreply.github.com>
2019-09-16 09:03:20 -0400

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,7 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8ae85ded77196ff3296367a775296a2d' #this is a key I generated from my python shell
+app.config['SECRET_KEY'] = '' #this is a key I generated from my python shell
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/mydatabase.db'
 db = SQLAlchemy(app)
 
